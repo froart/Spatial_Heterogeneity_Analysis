80d167c1d4f4322e3381b37fd13e917d11ac5749
Artemiy <artemiusxyz@gmail.com>
2025-06-25 17:59:57 +0200

    Initialization of the project

---
+++ sha.jl
@@ -0,0 +1,13 @@
+using GLMakie
+
+N = 40
+M = 40
+Landscape = Matrix{String}(undef, N, M)
+
+# TODO: initialization of several landscapes of different configurations
+
+# TODO: implement extended spatial entropy evaluator
+function H(L::Matrix{String})
+
+end
+
